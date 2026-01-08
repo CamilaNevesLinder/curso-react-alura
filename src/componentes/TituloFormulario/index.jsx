5567ae77d0c1bac0e374549d43fa96eb0cb18793
CamilaNevesLinder <camilaneveslindner@.com>
2026-01-08 09:59:20 -0300

    aula 4 e 5

--- src/componentes/TituloFormulario/index.jsx
+++ src/componentes/TituloFormulario/index.jsx
@@ -1,11 +1,6 @@
-import './titulo-formulario.estilos.css'
+import "./titulo-formulario.estilos.css";
 // props é um OBJETO
 // props.children
-export function TituloFormulario ({children}) {
-    return (
-      <h2 className='titulo-form'>
-        {children}
-      </h2>
-    )
+export function TituloFormulario({ children }) {
+  return <h2 className="titulo-form">{children}</h2>;
 }
-  
